c45f56f309f40a7ec0c34ab983a6b22e0c40fb7b
Swarup Nath (BASSETTI) <swarup.nath@bassetti-group.com>
2026-01-30 12:30:15 +0530

    Update AppTest.java
    Co-Authored-By: Arnab Ghosh (BASSETTI) <244881857+arnabghosh-Bassetti-group@users.noreply.github.com>

--- demo-java-practice/src/test/java/com/example/demo/AppTest.java
+++ demo-java-practice/src/test/java/com/example/demo/AppTest.java
@@ -7,7 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
 
     @Test
-    void testAdd() {
+    static boolean testAdd() {
         assertEquals(3, App.add(1, 2));
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello from Demo Java Project!");
+        System.out.println(AppTest.testAdd());
     }
 }
